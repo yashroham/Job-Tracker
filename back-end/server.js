2f2263546d1a6c287cbde7827e1de147e012f7bc
yashroham <yashroham7781@gmail.com>
2026-08-30 15:20:09 +0530

    put&patch added

--- back-end/server.js
+++ back-end/server.js
@@ -7,6 +7,14 @@ const PORT = 3000;
 app.use(express.json());
 
 let jobs = [];
+let nextId = 1;
+const allowedStatuses = [
+    "Applied",
+    "Interview",
+    "Rejected",
+    "Offer",
+    "Accepted"
+];
 
 // Home route
 app.get("/", (req, res) => {
@@ -15,9 +23,19 @@ app.get("/", (req, res) => {
 
 // Add a new job
 app.post("/api/jobs", (req, res) => {
+    const { company, role, status } = req.body;
+
+    if (!company || !role || !status) {
+        return res.status(400).json({
+            message: "Company, role, and status are required"
+        });
+    }
+
     const newJob = {
-        id: jobs.length + 1,
-        ...req.body
+        id: nextId++,
+        company,
+        role,
+        status
     };
 
     jobs.push(newJob);
@@ -30,9 +48,52 @@ app.get("/api/jobs", (req, res) => {
     res.json(jobs);
 });
 
-// Get one job by ID
+// Get a single job by ID
 app.get("/api/jobs/:id", (req, res) => {
-    const job = jobs.find((job) => job.id === Number(req.params.id));
+    const id = parseInt(req.params.id);
+
+    const job = jobs.find(job => job.id === id);
+
+    if (!job) {
+        return res.status(404).json({
+            message: "Job not found"
+        });
+    }
+
+    res.json(job);
+});
+
+app.put("/api/jobs/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const job = jobs.find((job) => job.id === id);
+
+    if (!job) {
+        return res.status(404).json({ message: "Job not found" });
+    }
+
+    job.status = req.body.status;
+
+    res.json(job);
+});
+
+// Partially update a job
+app.patch("/api/jobs/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const job = jobs.find((job) => job.id === id);
+
+    if (!job) {
+        return res.status(404).json({ message: "Job not found" });
+    }
+
+    if (req.body.status && !allowedStatuses.includes(req.body.status)) {
+        return res.status(400).json({
+            message: "Invalid job status"
+        });
+    }
+
+    Object.assign(job, req.body);
 
     res.json(job);
 });
@@ -50,6 +111,8 @@ app.delete("/api/jobs/:id", (req, res) => {
 
     res.send("Job deleted successfully");
 });
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
